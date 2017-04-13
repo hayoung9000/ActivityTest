ffac809d21bc1e3469fc7cec6a277ba22776333a
hayoung9000 <wjsgkdud9000@naver.com>
2017-04-13 19:59:14 +0900

    #2 Two Layout

--- app/src/main/java/com/example/lg/activitytest/SecondActivity.java
+++ app/src/main/java/com/example/lg/activitytest/SecondActivity.java
@@ -3,11 +3,11 @@ package com.example.lg.activitytest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SecondActicity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second_acticity);
+        setContentView(R.layout.activity_second);
     }
 }
